94c626405a0c3086e8293482e3ae0755e81b1a95
MhlengiNgubane <mhlengims@gmail.com>
2022-10-28 09:22:53 +0200

    .

--- go-movies/src/components/EditMovie.js
+++ go-movies/src/components/EditMovie.js
@@ -1,8 +1,8 @@
 import React, { Component, Fragment } from "react";
 import "./EditMovie.css";
-import Input from "./form-components/Input";
-import TextArea from "./form-components/TextArea";
-import Select from "./form-components/Select";
+import Input from './form-components/Input';
+import TextArea from './form-components/TextArea';
+import Select from './form-components/Select';
 
 export default class EditMovie extends Component {
   state = {
@@ -11,10 +11,12 @@ export default class EditMovie extends Component {
     error: null,
   };
 
+
   constructor(props) {
     super(props);
     this.state = {
       movie: {
+        id: 0,
         title: "",
         release_date: "",
         runtime: "",
@@ -23,15 +25,15 @@ export default class EditMovie extends Component {
         description: "",
       },
       mpaaOptions: [
-        { id: "G", value: "G" },
-        { id: "PG", value: "PG" },
-        { id: "PG13", value: "PG13" },
-        { id: "PG16", value: "PG16" },
-        { id: "R18", value: "R18" },
+        {id: "G", value: "G"},
+        {id: "PG", value: "PG"},
+        {id: "PG13", value: "PG13"},
+        {id: "R", value: "R"},
+        {id: "NC17", value: "NC17"},
       ],
       isLoaded: false,
       error: null,
-    };
+    }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -40,7 +42,7 @@ export default class EditMovie extends Component {
   handleSubmit = (evt) => {
     console.log("Form was submitted");
     evt.preventDefault();
-  };
+  }
 
   handleChange = (evt) => {
     let value = evt.target.value;
@@ -49,61 +51,59 @@ export default class EditMovie extends Component {
       movie: {
         ...prevState.movie,
         [name]: value,
-      },
-    }));
-  };
+      }
+    }))
+  }
 
-  componentDidMount() {}
+  componentDidMount() {
+
+  }
 
   render() {
     let { movie } = this.state;
+
     return (
       <Fragment>
         <h2>Add/Edit Movie</h2>
         <hr />
         <form onSubmit={this.handleSubmit}>
-          <input
+          <input 
             type="hidden"
             name="id"
             id="id"
             value={movie.id}
             onChange={this.handleChange}
-          />
+            />
 
           <Input
             title={"Title"}
-            type={"text"}
-            name={"title"}
+            type={'text'}
+            name={'title'}
             value={movie.title}
             handleChange={this.handleChange}
           />
 
           <Input
             title={"Release Date"}
-            type={"date"}
-            name={"release_date"}
+            type={'date'}
+            name={'release_date'}
             value={movie.release_date}
             handleChange={this.handleChange}
           />
 
           <Input
             title={"Runtime"}
-            type={"text"}
-            name={"runtime"}
+            type={'text'}
+            name={'runtime'}
             value={movie.runtime}
             handleChange={this.handleChange}
           />
 
-          {/*<div className="mb-3">
+          {/* <div className="mb-3">
             <label htmlFor="mpaa_rating" className="form-label">
               MPAA Rating
             </label>
-            <select
-              name="mpaa_rating"
-              className="form-select"
-              value={movie.mpaa_rating}
-              onChange={this.handleChange}
-            >
+            <select name="mpaa_rating" className="form-select" value={movie.mpaa_rating} onChange={this.handleChange}>
               <option className="form-select">Choose...</option>
               <option className="form-select" value="G">
                 G
@@ -121,31 +121,32 @@ export default class EditMovie extends Component {
                 NC17
               </option>
             </select>
-      </div> */}
-          <Select
-            title={"MPAA Rating"}
-            name={"mpaa_rating"}
+          </div> */}
+
+          <Select 
+            title={'MPAA Rating'}
+            name={'mpaa_rating'}
             options={this.state.mpaaOptions}
             value={movie.mpaa_rating}
             handleChange={this.handleChange}
-            placeholder={"Choose..."}
+            placeholder={'Choose...'}
           />
 
           <Input
             title={"Rating"}
-            type={"text"}
-            name={"rating"}
+            type={'text'}
+            name={'rating'}
             value={movie.rating}
             handleChange={this.handleChange}
           />
 
           <TextArea
-            title={"Description"}
-            name={"description"}
-            value={movie.description}
-            rows={"3"}
-            handleChange={this.handleChange}
-          />
+              title={"Description"}
+              name={"description"}
+              value={movie.description}
+              rows={"3"}
+              handleChange={this.handleChange}
+            />
 
           <hr />
 
--- go-movies/src/components/form-components/Select.js
+++ go-movies/src/components/form-components/Select.js
@@ -1,29 +1,29 @@
 const Select = (props) => {
   return (
     <div className="mb-3">
-      <label htmlFor="mpaa_rating" className="form-label">
+      <label htmlFor={props.name} className="form-label">
         {" "}
         {props.title}{" "}
       </label>
       <select
-        name={props.name}
         className="form-select"
+        name={props.name}
         value={props.value}
-        onChange={this.handleChange}
+        onChange={props.handleChange}
       >
-        <option value="">{props.placeholder}</option>
-        {props.option.map((option) => {
-          return (
-            <option
-              className="form-select"
-              key={option.id}
-              value={option.id}
-              label={option.value}
-            >
-              {option.value}
-            </option>
-          );
-        })}
+          <option value="">{props.placeholder}</option>
+          {props.options.map((option) => {
+              return (
+                  <option
+                    className="form-select"
+                    key={option.id}
+                    value={option.id}
+                    label={option.value}
+                    >
+                        {option.value}
+                    </option>
+              )
+          })}
       </select>
     </div>
   );
